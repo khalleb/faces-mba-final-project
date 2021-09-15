0595d7326da2075f717afe8e57d47bd8ce27e408
khalleb <khallebw@gmail.com>
2021-09-15 20:25:47 -0300

    .env

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,24 @@
+import 'dotenv/config';
+
 import { existsSync, mkdirSync, copyFile } from 'fs';
 import { getFile, readFileGetGroup } from './lib/files';
 import { pathWindowsLinux } from './lib/util';
 
 (async (): Promise<void> => {
   console.log('Carregando...');
-  const basePath = '/home/khalleb/Documents/base-data-faces'
-  const imageFile = await getFile(`${basePath}${pathWindowsLinux()}identity_CelebA.txt`);
+  const basePath = process?.env?.BASE_PATH;
+  if (!basePath || !existsSync(basePath)) {
+    throw new Error('Pasta de arquivos padrão não encontrada');
+  }
+  const fileList = `${basePath}${pathWindowsLinux()}identity_CelebA.txt`;
+  const folderImages = `${basePath}${pathWindowsLinux()}img_align_celeba`
+  if (!existsSync(fileList)) {
+    throw new Error('Arquivo de texto não localizado');
+  }
+  if (!existsSync(folderImages)) {
+    throw new Error('Pasta de imagens não localizadas');
+  }
+  const imageFile = await getFile(fileList);
   if (imageFile) {
     const groupBy = await readFileGetGroup(imageFile);
     if (groupBy) {
@@ -15,7 +28,7 @@ import { pathWindowsLinux } from './lib/util';
       }
       try {
         groupBy.forEach(file => {
-          const fileOld = `${basePath}${pathWindowsLinux()}img_align_celeba${pathWindowsLinux()}${file.image}`
+          const fileOld = `${folderImages}${pathWindowsLinux()}${file.image}`
           const fileNew = `${folferNewImages}${pathWindowsLinux()}${file.group}_${file.image}`
           copyFile(fileOld, fileNew, (err) => {
             if (err) {
